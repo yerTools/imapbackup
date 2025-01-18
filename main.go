afbc5d076b0a86e1e40fc97ccc49f68faa7fe2ab
Felix Mayer <FelixM@yer.tools>
2025-01-18 22:40:40 +0100

    Add PocketBase

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// registers new "GET /hello" route
+		se.Router.GET("/hello", func(re *core.RequestEvent) error {
+			return re.String(200, "Hello world!")
+		})
+
+		return se.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
